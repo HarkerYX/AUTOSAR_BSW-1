2ff10cf74b11e13db45f909d8e4eedbf52b28ed3
Yiyang-CYY <yiyang.cai@pm.me>
2023-02-06 11:04:16 +0800

    Add CanIf_Hth_Can_Controller_Config

--- Config/inc/CanIf_LCfg.h
+++ Config/inc/CanIf_LCfg.h
@@ -12,5 +12,23 @@
 
 #ifndef AUTOSAR_LIB_CANIF_LCFG_H
 #define AUTOSAR_LIB_CANIF_LCFG_H
+/***********************************************************************************************************************
+*                                                   Includes                                                           *
+***********************************************************************************************************************/
+#include "../../Std_Types/StandardTypes.h"
+#include "../../Can_GeneralTypes/Can_GeneralTypes.h"
+#include "../../ComStack_Types/ComStackTypes.h"
+#include "../../EcuM/EcuM.h"
+
+/***********************************************************************************************************************
+*                                               Type definitions                                                       *
+***********************************************************************************************************************/
+typedef struct {
+    /* This id is for HTH object in Can controller */
+    Can_HwHandleType CanIf_Hth_HW_Id;
+
+    /* Actual Can controller Id */
+    uint8            CanIf_Can_Controlelr_Id;
+} CanIf_Hth_Can_Controller_Config;
 
 #endif //AUTOSAR_LIB_CANIF_LCFG_H
