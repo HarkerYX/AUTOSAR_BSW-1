a4cb53df4112ad9c660575a8c4f4d681d542c3a2
Yiyang-CYY <yiyang.cai@pm.me>
2023-02-08 15:09:04 +0800

    SWS_CANIF_00750

--- CanIf/src/CanIf.c
+++ CanIf/src/CanIf.c
@@ -28,6 +28,7 @@ Std_ReturnType CanIf_Transmit(PduIdType TxPduId, const PduInfoType *PduInfoPtr)
     uint8                   Can_Object_Id;
     CanIf_PduModeType       Pdu_Mode;
     Can_ControllerStateType Controller_Mode;
+    boolean                 Do_Transmit = FALSE;
 
     Can_Controller_Id = CanIf_Tx_Pdu_Cfg_Group_1[TxPduId]
         .CanIf_Buffer_Cfg_Ref
@@ -37,31 +38,50 @@ Std_ReturnType CanIf_Transmit(PduIdType TxPduId, const PduInfoType *PduInfoPtr)
 
     Can_Return = CanIf_GetPduMode(Can_Controller_Id, &Pdu_Mode);
 
-
     /* SWS_CANIF_00317 */
     if (Can_Return == E_OK) {
         Can_Return = CanIf_GetControllerMode(Can_Controller_Id, &Controller_Mode);
 
         if (Controller_Mode == CAN_CS_STARTED) {
             switch (Pdu_Mode) {
-                case CANIF_OFFLINE:
+                case CANIF_OFFLINE: {
                     /* SWS_CANIF_00382 */
                     /* DET will report error */
                     Can_Return = E_NOT_OK;
+                    Do_Transmit = FALSE;
                     break;
-                case CANIF_TX_OFFLINE_ACTIVE:
+                }
+
+                case CANIF_TX_OFFLINE_ACTIVE: {
                     /* SWS_CANIF_00072 */
                     if (CanIf_Group_1[0].CanIf_Public_Cfg_Ref->CanIf_Tx_Offline_Active_Support == TRUE) {
                         CanIf_TxConfirmation(TxPduId);
+                        Do_Transmit = TRUE;
                     }
                     else {
                         Can_Return = E_NOT_OK;
                     }
+
                     break;
-                case CANIF_ONLINE:
+                }
+                case CANIF_ONLINE: {
+                    /* SWS_CANIF_00747, SWS_CANIF_00750*/
+                    if (CanIf_Group_1[0].CanIf_Public_Cfg_Ref->CanIf_Public_Pn_Support == STD_ON) {
+                        if (CanIf_Tx_Pdu_Cfg_Group_1[TxPduId].CanIf_Tx_Pdu_Pn_Filter_Pdu != STD_ON) {
+                            Do_Transmit = FALSE;
+                        }
+                    }
+                    else {
+                        Do_Transmit = TRUE;
+                    }
                     break;
+                }
                 case CANIF_TX_OFFLINE:
                     break;
+                default: {
+                    Can_Return = E_NOT_OK;
+                    break;
+                }
             }
         }
 
@@ -71,8 +91,13 @@ Std_ReturnType CanIf_Transmit(PduIdType TxPduId, const PduInfoType *PduInfoPtr)
         if (Controller_Mode == CAN_CS_UNINIT) Can_Return = E_NOT_OK;
 
         if (Controller_Mode == CAN_CS_STOPPED) {
-
+            /* SWS_CANIF_00677 */
+            Can_Return = E_NOT_OK;
         }
+    }
+
+    if ((Can_Return == E_OK) && (Do_Transmit == TRUE)) {
+
     }
 
     return Can_Return;
